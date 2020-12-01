ba17e08fd7ce8e96e2c0eff52e3d2bfcf7b45082
BoussonKarel <BoussonKarel@users.noreply.github.com>
2020-12-01 21:14:01 +0100

    Finished app.html (for now?)

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 let htmlInfo, btnExpand, htmlInfoDetails;
-let htmlCountdown, htmlDate, htmlName, htmlOrganisation, htmlMission, htmlRocket, htmlPad;
+let htmlCountdown, htmlProgressBar, htmlDate, htmlName, htmlOrganisation, htmlMission, htmlRocket, htmlPad;
 
 const listenToExpand = function() {
     btnExpand.addEventListener("click", function() {
@@ -14,9 +14,61 @@ const listenToExpand = function() {
 
 const showCountdown = function(launchTime) {
     date = new Date(launchTime);
-    now = new Date()
-    htmlCountdown.innerHTML = "T- 00:00:00";
-    htmlDate.innerHTML = date;
+    htmlDate.innerHTML = `${formatDate(date)}`;
+
+    const MaxBarInMillis = 12 * 60 * 60 * 1000; /* 12 hours */
+
+    var x = setInterval(function() {
+        let T;
+        now = new Date()
+        let diff = date - now;
+        let percentage;
+
+        if (diff < 0) {
+            T = "T+";
+            diff = Math.abs(diff);
+            percentage = 100;
+        }
+        else {
+            T = "T-"
+            
+            percentage = 100 - Math.round(diff / MaxBarInMillis * 100 *10) /10;
+
+            if (percentage < 0) {
+                percentage = 0;
+            }
+        }
+
+        htmlCountdown.innerHTML = `${T} ${toHHMMSS(diff)}`;
+        
+        htmlProgressBar.style.width = `${percentage}%`;
+    }, 1000);
+}
+
+const formatDate = function(date) {
+    return `${date.toLocaleDateString()} at ${addLeadingZero(date.getHours())}:${addLeadingZero(date.getMinutes())}`
+}
+
+const toHHMMSS = function(timediff) {
+    const totalSeconds = Math.floor(timediff/1000);
+
+    let days = addLeadingZero(Math.floor(timediff / (1000 * 60 * 60 * 24)));
+    let hours = addLeadingZero(Math.floor((timediff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    let minutes = addLeadingZero(Math.floor((timediff % (1000 * 60 * 60)) / (1000 * 60)));
+    let seconds = addLeadingZero(Math.floor((timediff % (1000 * 60)) / 1000));
+    return `${days}:${hours}:${minutes}:${seconds}`;
+}
+
+const addLeadingZero = function(number) {
+    if (number <= 0) {
+        return "00";
+    }
+    else if (number < 10) {
+        return `0${number}`;
+    }
+    else {
+        return `${number}`;
+    }
 }
 
 const showLaunchInfo = function(data) {
@@ -33,6 +85,8 @@ const getLaunchInfo = async () => {
     const endpoint = `https://lldev.thespacedevs.com/2.1.0/launch/upcoming`;
 
     const launchData = await getData(endpoint);
+
+    console.log(launchData.results);
     
     try {
         showLaunchInfo(launchData.results[0]);
@@ -56,6 +110,7 @@ const init = function() {
     htmlInfo = document.querySelector(".js-info");
     btnExpand = document.querySelector(".js-expand");
     htmlCountdown = document.querySelector(".js-countdown");
+    htmlProgressBar = document.querySelector(".js-bar");
     htmlDate = document.querySelector(".js-date");
     htmlName = document.querySelector(".js-name");
     htmlOrganisation = document.querySelector(".js-organisation");
