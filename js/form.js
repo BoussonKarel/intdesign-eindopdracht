01cc9e4513411e7674901f34787b82c2f3914aba
BoussonKarel <BoussonKarel@users.noreply.github.com>
2020-12-01 17:56:49 +0100

    Finished floating label

--- js/form.js
+++ js/form.js
@@ -3,14 +3,11 @@ let htmlFormfields;
 const handleFloatingLabel = function() {
     htmlFormfields = document.querySelectorAll(".js-form-field");
     for (const formfield of htmlFormfields) {
-        const label = formfield.querySelector(".js-label");
         const input = formfield.querySelector(".js-input");
-        console.log(label, input);
 
         input.addEventListener("blur", function() {
             if (!input.value || !input.value.length) {
                 formfield.classList.remove("is-floating");
-                console.log(formfield);
             }
             else {
                 formfield.classList.add("is-floating");
